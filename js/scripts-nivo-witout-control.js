8991adff634b8e471164e8f4043cf9118c9f47fd
Efrix <info@efrix.net>
2016-03-25 19:44:59 -0600

    first commit

---
+++ js/scripts-nivo-witout-control.js
@@ -0,0 +1,141 @@
+// NIVO SLIDER
+jQuery(document).ready(function($){
+  $(window).load(function() {
+      $('#slider').nivoSlider({
+        controlNav: false, 
+      });
+  });
+
+});
+
+//MENU toggle-menu
+$('#toggle-menu').click(function(){
+  $(this).next().slideToggle();
+});
+// SCROLL SPEED
+$(function() {
+  jQuery.scrollSpeed(60, 400);
+});
+
+// TOP BUTTON
+$(document).ready(function() {
+
+                $.apScrollTop({
+                    'onInit': function(evt) {
+                        console.log('apScrollTop: init');
+                    }
+                });
+
+                // Add event listeners
+                $.apScrollTop().on('apstInit', function(evt) {
+                    console.log('apScrollTop: init');
+                });
+
+                $.apScrollTop().on('apstToggle', function(evt, details) {
+                    console.log('apScrollTop: toggle / is visible: ' + details.visible);
+                });
+
+                $.apScrollTop().on('apstCssClassesUpdated', function(evt) {
+                    console.log('apScrollTop: cssClassesUpdated');
+                });
+
+                $.apScrollTop().on('apstPositionUpdated', function(evt) {
+                    console.log('apScrollTop: positionUpdated');
+                });
+
+                $.apScrollTop().on('apstEnabled', function(evt) {
+                    console.log('apScrollTop: enabled');
+                });
+
+                $.apScrollTop().on('apstDisabled', function(evt) {
+                    console.log('apScrollTop: disabled');
+                });
+
+                $.apScrollTop().on('apstBeforeScrollTo', function(evt, details) {
+                    console.log('apScrollTop: beforeScrollTo / position: ' + details.position + ', speed: ' + details.speed);
+
+                    // You can return a single number here, which means that to this position
+                    // browser window scolls to
+                    /*
+                    return 100;
+                    */
+
+                    // .. or you can return an object, containing position and speed:
+                    /*
+                    return {
+                        position: 100,
+                        speed: 100
+                    };
+                    */
+
+                    // .. or do not return anything, so the default values are used to scroll
+                });
+
+                $.apScrollTop().on('apstScrolledTo', function(evt, details) {
+                    console.log('apScrollTop: scrolledTo / position: ' + details.position);
+                });
+
+                $.apScrollTop().on('apstDestroy', function(evt, details) {
+                    console.log('apScrollTop: destroy');
+                });
+
+            });
+
+
+            // Add change events for options
+            $('#option-enabled').on('change', function() {
+                var enabled = $(this).is(':checked');
+                $.apScrollTop('option', 'enabled', enabled);
+            });
+
+            $('#option-visibility-trigger').on('change', function() {
+                var value = $(this).val();
+                if (value == 'custom-function') {
+                    $.apScrollTop('option', 'visibilityTrigger', function(currentYPos) {
+                        var imagePosition = $('#image-for-custom-function').offset();
+                        return (currentYPos > imagePosition.top);
+                    });
+                }
+                else {
+                    $.apScrollTop('option', 'visibilityTrigger', parseInt(value));
+                }
+            });
+
+            $('#option-visibility-fade-speed').on('change', function() {
+                var value = parseInt($(this).val());
+                $.apScrollTop('option', 'visibilityFadeSpeed', value);
+            });
+
+            $('#option-scroll-speed').on('change', function() {
+                var value = parseInt($(this).val());
+                $.apScrollTop('option', 'scrollSpeed', value);
+            });
+
+            $('#option-position').on('change', function() {
+                var value = $(this).val();
+                $.apScrollTop('option', 'position', value);
+            });
+
+
+
+
+$('.maps').click(function () {
+    $('.maps iframe').css("pointer-events", "auto");
+});
+
+//OWL JS
+
+
+$(document).ready(function() {
+
+  $("#owl-demo").owlCarousel({
+
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+      items : 4,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+
+  });
+
+});
